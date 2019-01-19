c9b460be422684b667181005c93a2a7f31a8cfe3
wustmz <865011721@qq.com>
2019-01-19 17:26:03 +0800

    基金管理

--- src/main/java/org/mz/controller/IndexController.java
+++ src/main/java/org/mz/controller/IndexController.java
@@ -1,12 +1,14 @@
 package org.mz.controller;
 
 import com.google.gson.Gson;
+import org.mz.entity.Fund;
 import org.mz.entity.FundTx;
+import org.mz.service.FundService;
 import org.mz.service.FundTxService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,12 +17,15 @@ public class IndexController {
 
     @Autowired
     private FundTxService fundTxService;
+    @Autowired
+    private FundService fundService;
 
     /**
      * 交易列表
+     *
      * @return
      */
-    @GetMapping("/txList")
+    @GetMapping("/tx/list")
     public String txList() {
         List<FundTx> all = fundTxService.findAll();
         return "txList";
@@ -28,11 +33,32 @@ public class IndexController {
 
     /**
      * 基金列表
+     *
      * @return
      */
-    @GetMapping("/fundList")
-    public String fundList() {
-        List<FundTx> all = fundTxService.findAll();
+    @GetMapping("/fund/list")
+    public String fundList(Model model) {
+        List<Fund> list = fundService.findAll();
+        model.addAttribute("list", list);
+        return "fundList";
+    }
+
+    @GetMapping("/fund/info/{id}")
+    public String fundInfo(@PathVariable int id, Model model) {
+        Fund fund = fundService.findById(id);
+        model.addAttribute("fund", fund);
+        return "fundList";
+    }
+
+    @PostMapping("/fund/save")
+    public String saveFund(@RequestBody Fund fund) {
+        fundService.save(fund);
+        return "fundList";
+    }
+
+    @PostMapping("/fund/delById/{id}")
+    public String delById(@PathVariable int id) {
+        fundService.deleteById(id);
         return "fundList";
     }
 }
