0998f9901716a2d33697bdab6918b3827e1a4c79
wustmz <865011721@qq.com>
2019-01-19 13:20:43 +0800

    init

---
+++ src/main/java/org/mz/FreedomofwealthApplication.java
@@ -0,0 +1,14 @@
+package org.mz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreedomofwealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreedomofwealthApplication.class, args);
+    }
+
+}
+
