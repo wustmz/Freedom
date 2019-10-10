ba085e0bff5c30ac30333b256bae28a5ce3f54bf
wustmz <865011721@qq.com>
2019-10-10 15:26:42 +0800

    添加图表

--- src/main/java/org/mz/controller/IndexController.java
+++ src/main/java/org/mz/controller/IndexController.java
@@ -3,8 +3,10 @@ package org.mz.controller;
 import org.mz.common.FundUtil;
 import org.mz.common.MathUtil;
 import org.mz.common.XirrUtils;
+import org.mz.entity.Finance;
 import org.mz.entity.FundTx;
 import org.mz.entity.Tx;
+import org.mz.service.FinanceService;
 import org.mz.service.FundTxService;
 import org.mz.service.TxService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +15,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Controller
 public class IndexController {
@@ -25,49 +26,62 @@ public class IndexController {
     private TxService txService;
     @Autowired
     private FundTxService fundTxService;
+    @Autowired
+    private FinanceService financeService;
 
     @RequestMapping("/")
     public String index(Model model) {
-        List<FundTx> all = fundTxService.findAll();
-        Map<String, Object> xirrInfo = XirrUtils.getXirrInfo(all);
-        double amount = (double) xirrInfo.get("total");
+        List<Finance> finances = financeService.findAll();
 
-        //总价值
-        model.addAttribute("amount", MathUtil.getDouble2(amount));
-        //总投入
-        model.addAttribute("invest", xirrInfo.get("invest"));
-        //总投入年收益率
-        model.addAttribute("totalXirr", xirrInfo.get("xirr"));
-
-        Map<String, Object> map = new HashMap<>();
-        for (FundTx fundTx : all) {
-            map.put(fundTx.getCode(), fundTx.getName());
+        for (Finance finance : finances) {
+            BigDecimal total = finance.getTotal();
+            Date updatetime = finance.getUpdatetime();
         }
-
-        //基金信息汇总
-        List<Map<String, Object>> pages = new ArrayList<>();
-        Map<String, Object> page;
-        for (String s : map.keySet()) {
-            page = new HashMap<>();
-            List<FundTx> txList = fundTxService.findFundTxByCode(s);
-            Map<String, Object> info = XirrUtils.getXirrInfo(txList);
-            page.put("code", s);
-            page.put("name", map.get(s));
-            double total = (double) info.get("total");
-            page.put("total", MathUtil.getDouble2(total));
-            page.put("ratio", MathUtil.getRate(total / amount));
-            page.put("xirr", info.get("xirr"));
-            String xirr = (String) info.get("xirr");
-            if (!xirr.contains("-")) {
-                page.put("color", "red");
-            } else {
-                page.put("color", "green");
-            }
-            pages.add(page);
-        }
-        model.addAttribute("pages", pages);
         return "index";
     }
+
+//    @RequestMapping("/")
+//    public String index(Model model) {
+//        List<FundTx> all = fundTxService.findAll();
+//        Map<String, Object> xirrInfo = XirrUtils.getXirrInfo(all);
+//        double amount = (double) xirrInfo.get("total");
+//
+//        //总价值
+//        model.addAttribute("amount", MathUtil.getDouble2(amount));
+//        //总投入
+//        model.addAttribute("invest", xirrInfo.get("invest"));
+//        //总投入年收益率
+//        model.addAttribute("totalXirr", xirrInfo.get("xirr"));
+//
+//        Map<String, Object> map = new HashMap<>();
+//        for (FundTx fundTx : all) {
+//            map.put(fundTx.getCode(), fundTx.getName());
+//        }
+//
+//        //基金信息汇总
+//        List<Map<String, Object>> pages = new ArrayList<>();
+//        Map<String, Object> page;
+//        for (String s : map.keySet()) {
+//            page = new HashMap<>();
+//            List<FundTx> txList = fundTxService.findFundTxByCode(s);
+//            Map<String, Object> info = XirrUtils.getXirrInfo(txList);
+//            page.put("code", s);
+//            page.put("name", map.get(s));
+//            double total = (double) info.get("total");
+//            page.put("total", MathUtil.getDouble2(total));
+//            page.put("ratio", MathUtil.getRate(total / amount));
+//            page.put("xirr", info.get("xirr"));
+//            String xirr = (String) info.get("xirr");
+//            if (!xirr.contains("-")) {
+//                page.put("color", "red");
+//            } else {
+//                page.put("color", "green");
+//            }
+//            pages.add(page);
+//        }
+//        model.addAttribute("pages", pages);
+//        return "index";
+//    }
 
     @RequestMapping("/fundList/{code}")
     public String getFundList(@PathVariable String code, Model model) {
--- src/test/java/org/mz/AppTest.java
+++ src/test/java/org/mz/AppTest.java
@@ -225,7 +225,9 @@ public class AppTest {
 
     @Test
     public void testArray() {
-
+        BigDecimal b1 = new BigDecimal(5);
+        BigDecimal b2 = new BigDecimal(3);
+        System.out.println(b1.subtract(b2));
     }
 
 }
