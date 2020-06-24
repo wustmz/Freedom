99e4efba5721623335feb888f5fe88779adabee0
wustmz <865011721@qq.com>
2020-06-24 21:57:00 +0800

    init

--- src/main/java/org/mz/mapper/FinanceMapper.java
+++
@@ -1,9 +0,0 @@
-package org.mz.mapper;
-
-import com.baomidou.mybatisplus.mapper.BaseMapper;
-import org.mz.entity.Finance;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface FinanceMapper extends BaseMapper<Finance> {
-}
--- src/main/java/org/mz/mapper/TxMapper.java
+++
@@ -1,9 +0,0 @@
-package org.mz.mapper;
-
-import com.baomidou.mybatisplus.mapper.BaseMapper;
-import org.mz.entity.Tx;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface TxMapper extends BaseMapper<Tx> {
-}
