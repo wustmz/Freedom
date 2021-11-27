ab35a083a07275c4173703d90cd2fbaed86853c5
wustmz <865011721@qq.com>
2021-11-27 21:11:57 +0800

    add 类加载器demo

---
+++ jvm/src/main/java/org/mz/TestJDKClassLoader.java
@@ -0,0 +1,41 @@
+package org.mz;
+
+import sun.misc.Launcher;
+
+import java.net.URL;
+
+/**
+ * 类加载器
+ */
+public class TestJDKClassLoader {
+    public static void main(String[] args) {
+        //类加载器是C++生成的，所以打印出来是null，涉及到跨语言调用
+        //sun.misc.Launcher
+        System.out.println(String.class.getClassLoader());
+        System.out.println(com.sun.crypto.provider.DESKeyFactory.class.getClassLoader().getClass().getName());
+        System.out.println(TestJDKClassLoader.class.getClassLoader().getClass().getName());
+
+        System.out.println();
+        ClassLoader appClassLoader = ClassLoader.getSystemClassLoader();
+        ClassLoader extClassloader = appClassLoader.getParent();
+        ClassLoader bootstrapLoader = extClassloader.getParent();
+        System.out.println("the bootstrapLoader : " + bootstrapLoader);
+        System.out.println("the extClassloader : " + extClassloader);
+        System.out.println("the appClassLoader : " + appClassLoader);
+
+        System.out.println();
+        System.out.println("bootstrapLoader加载以下文件：");
+        URL[] urls = Launcher.getBootstrapClassPath().getURLs();
+        for (URL url : urls) {
+            System.out.println(url);
+        }
+
+        System.out.println();
+        System.out.println("extClassloader加载以下文件：");
+        System.out.println(System.getProperty("java.ext.dirs"));
+
+        System.out.println();
+        System.out.println("appClassLoader加载以下文件：");
+        System.out.println(System.getProperty("java.class.path"));
+    }
+}
